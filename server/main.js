ed90319c3ffda124e101e27356f457806122f6b7
Yile Liu <yile.liu@autodesk.com>
2018-01-26 13:41:22 +0800

    step 1: add required packages

---
+++ server/main.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+
+SimpleSchema.extendOptions(['autoform']);
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+});
