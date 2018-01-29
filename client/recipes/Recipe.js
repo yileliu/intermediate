91c3aa3924a7dcbebf883e89121b62da66b16acc
Yile Liu <yile.liu@autodesk.com>
2018-01-29 13:42:39 +0800

    step 17: set template property with reactivevar

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,5 +1,12 @@
 import { Template } from "meteor/templating";
 import { Session } from "meteor/session";
+import { ReactiveVar } from "meteor/reactive-var";
+
+Template.Recipe.onCreated(function(){
+    this.editMode = new ReactiveVar(false);
+    // this.editMode = new ReactiveVar();
+    // this.editMode.set(false);
+});
 
 Template.Recipe.events({
     'click .toggle-menu': function () {
@@ -8,13 +15,16 @@ Template.Recipe.events({
     'click .fa-trash'(){
        Meteor.call('deleteRecipe', this._id);
     },
-    'click .fa-pencil'(){
-       Session.set('editMode', !Session.get('editMode'));
+    'click .fa-pencil'(event, template){
+        template.editMode.set(!template.editMode.get())
      }
 });
 
 Template.Recipe.helpers({
     updateRecipeId() {
         return this._id;
+    },
+    editMode: function(){
+        return Template.instance().editMode.get();
     }
 })
