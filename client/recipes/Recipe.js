1ff12c493bc6eb605311bd8bf9c9e59711308f61
Yile Liu <yile.liu@autodesk.com>
2018-01-29 12:51:16 +0800

    step 15: a quick update form to update recipe and auto save

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -4,4 +4,10 @@ Template.Recipe.events({
     'click .toggle-menu': function () {
         Meteor.call('toggleMenuItem', this._id, this.inMenu);
     }
+});
+
+Template.Recipe.helpers({
+    updateRecipeId() {
+        return this._id;
+    }
 })
