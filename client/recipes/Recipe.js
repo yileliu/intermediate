a0db02e8a8b532bd4ab4bd9d1c0e457ecd5ca633
Yile Liu <yile.liu@autodesk.com>
2018-01-29 13:37:03 +0800

    step 16: toggle remove and edit

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,9 +1,16 @@
 import { Template } from "meteor/templating";
+import { Session } from "meteor/session";
 
 Template.Recipe.events({
     'click .toggle-menu': function () {
         Meteor.call('toggleMenuItem', this._id, this.inMenu);
-    }
+    },
+    'click .fa-trash'(){
+       Meteor.call('deleteRecipe', this._id);
+    },
+    'click .fa-pencil'(){
+       Session.set('editMode', !Session.get('editMode'));
+     }
 });
 
 Template.Recipe.helpers({
